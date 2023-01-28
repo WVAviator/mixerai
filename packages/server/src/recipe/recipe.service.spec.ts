c98f1d470c8fb963afbac38f623051be2791aaed
Wvaviator <wvaviator@gmail.com>
2023-01-27 21:02:14 -0600

    fix recipe testing modules to remove errors

--- packages/server/src/recipe/recipe.service.spec.ts
+++ packages/server/src/recipe/recipe.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MockModel } from '../utils/testing/mock.model';
 import { RecipeService } from './recipe.service';
 
 describe('RecipeService', () => {
@@ -6,7 +7,13 @@ describe('RecipeService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RecipeService],
+      providers: [
+        RecipeService,
+        {
+          provide: 'RecipeModel',
+          useValue: MockModel,
+        },
+      ],
     }).compile();
 
     service = module.get<RecipeService>(RecipeService);
