20f427b863c8df8a3f51d2a3ead01d48bf6d323f
Wvaviator <wvaviator@gmail.com>
2023-02-02 20:04:56 -0600

    backend updates to authentication routes

--- packages/server/src/auth/auth.controller.ts
+++ packages/server/src/auth/auth.controller.ts
@@ -6,7 +6,7 @@ import {
   Response,
   UseGuards,
 } from '@nestjs/common';
-import { CookieOptions, Response as ExpressResponse } from 'express';
+import { Response as ExpressResponse } from 'express';
 import { User as UserModel } from '../user/schemas/user.schema';
 import { User } from '../user/user.decorator';
 import { AuthService } from './auth.service';
@@ -24,54 +24,33 @@ export class AuthController {
     this.logger.log('Redirecting to Google for authentication.');
   }
 
+  @Get('google/login')
+  @UseGuards(GoogleOAuthGuard)
+  async googleAuthLogin(
+    @User() user: UserModel,
+    @Response({ passthrough: false }) response: ExpressResponse,
+  ) {
+    this.logger.log(`Processing Google login for: ${user.email}`);
+    const { token, userData } = await this.authService.signIn(user);
+
+    return response.set('mixerai_token', token).json({ user: userData });
+  }
+
   @Get('google/callback')
   @UseGuards(GoogleOAuthGuard)
   @Redirect()
   async googleAuthRedirect(
     @User() user: UserModel,
-    @Response({ passthrough: true }) res: ExpressResponse,
+    @Response({ passthrough: false }) res: ExpressResponse,
   ) {
     this.logger.log(`Processing Google OAuth callback for: ${user.email}`);
-    const { token, userData } = await this.authService.signIn(user);
-
-    const cookieOptions: CookieOptions = {
-      httpOnly: process.env.NODE_ENV === 'production',
-      secure: process.env.NODE_ENV === 'production',
-      signed: true,
-      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-    };
-
-    this.logger.log(
-      `Obtained JWT token for: ${
-        user.email
-      }. Setting signed cookie with options: ${JSON.stringify(cookieOptions)}`,
-    );
-
-    res.cookie('mixerai_access_token', token, {
-      httpOnly: process.env.NODE_ENV === 'production',
-      secure: process.env.NODE_ENV === 'production',
-      signed: true,
-      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-    });
 
     const redirectUrl = new URL(
       process.env.AUTH_REDIRECT_DEEP_LINK || 'mixerai://',
     );
 
-    const redirectParams = new URLSearchParams({
-      id: userData.id,
-      displayName: userData.displayName,
-      avatarUrl: userData.avatarUrl,
-      email: userData.email,
-    });
-
-    redirectUrl.search = redirectParams.toString();
-
     this.logger.log(`Redirecting to: ${redirectUrl.toString()}`);
 
-    return {
-      statusCode: 302,
-      url: redirectUrl.toString(),
-    };
+    return res.redirect(redirectUrl.toString());
   }
 }
