f8f00f9fdaa96fd7952b750593edf48c78663a6a
Wvaviator <wvaviator@gmail.com>
2023-03-01 14:14:16 -0600

    fix additional subtract argument

--- packages/server/src/feed/feed.service.ts
+++ packages/server/src/feed/feed.service.ts
@@ -51,7 +51,6 @@ export class FeedService {
                       },
                     },
                   },
-                  ,
                   {
                     $size: {
                       $filter: {
