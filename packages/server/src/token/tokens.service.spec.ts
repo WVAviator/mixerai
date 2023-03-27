ed35423701ff59196d486943bf427ec5f6f42314
Wvaviator <wvaviator@gmail.com>
2023-03-26 20:04:52 -0500

    add tests for tokens service

--- packages/server/src/token/tokens.service.spec.ts
+++ packages/server/src/token/tokens.service.spec.ts
@@ -1,9 +1,14 @@
+import { RecipeDocument } from './../../dist/recipe/schemas/recipe.schema.d';
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Model } from 'mongoose';
+import { UserLoginEvent } from '../auth/events/user-login.event';
+import { UserCreatedEvent } from '../user/events/user-created.event';
+import { UserDocument } from '../user/schemas/user.schema';
 import { MockModel } from '../utils/testing/mock.model';
 import { TokenCount, TokenCountDocument } from './schemas/token-count.schema';
 import { TokensService } from './tokens.service';
+import { RecipeCreatedEvent } from '../recipe/events/recipe-created.event';
 
 describe('TokensService', () => {
   let tokensService: TokensService;
@@ -23,8 +28,52 @@ describe('TokensService', () => {
     );
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(tokensService).toBeDefined();
+  });
+
+  describe('handleUserCreatedEvent', () => {
+    it('should create a token count for a new user', async () => {
+      await tokensService.handleUserCreatedEvent(
+        new UserCreatedEvent({ id: '123' } as UserDocument),
+      );
+
+      expect(mockTokenCountModel.create).toHaveBeenCalledWith({
+        userId: '123',
+        tokens: 3,
+      });
+    });
+  });
+
+  describe('handleUserLoginEvent', () => {
+    it('should do nothing if a count already exists', async () => {
+      jest
+        .spyOn(mockTokenCountModel, 'findOne')
+        .mockResolvedValue({ userId: '123', tokens: 4 });
+
+      await tokensService.handleUserLoginEvent(
+        new UserLoginEvent({ id: '123' } as UserDocument),
+      );
+
+      expect(mockTokenCountModel.create).not.toHaveBeenCalled();
+    });
+
+    it('should create a token count for a user without one', async () => {
+      jest.spyOn(mockTokenCountModel, 'findOne').mockResolvedValue(null);
+
+      await tokensService.handleUserLoginEvent(
+        new UserLoginEvent({ id: '123' } as UserDocument),
+      );
+
+      expect(mockTokenCountModel.create).toHaveBeenCalledWith({
+        userId: '123',
+        tokens: 3,
+      });
+    });
   });
 
   describe('getTokenCount', () => {
@@ -52,6 +101,27 @@ describe('TokensService', () => {
       expect(findOneAndUpdateFunction).toBeCalledWith(
         { userId: '123' },
         { $inc: { tokens: 10 } },
+        { new: true },
+      );
+    });
+  });
+
+  describe('handleRecipeCreatedEvent', () => {
+    it('should decrement the token count by one when a recipe is created', async () => {
+      jest.spyOn(mockTokenCountModel, 'findOneAndUpdate').mockResolvedValue({
+        tokens: 10,
+      });
+
+      await tokensService.handleRecipeCreatedEvent(
+        new RecipeCreatedEvent(
+          { id: '456' } as RecipeDocument,
+          { id: '123' } as UserDocument,
+        ),
+      );
+
+      expect(mockTokenCountModel.findOneAndUpdate).toBeCalledWith(
+        { userId: '123' },
+        { $inc: { tokens: -1 } },
         { new: true },
       );
     });
--- packages/server/src/token/tokens.service.ts
+++ packages/server/src/token/tokens.service.ts
@@ -6,6 +6,7 @@ import {
 import { OnEvent } from '@nestjs/event-emitter';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { UserLoginEvent } from '../auth/events/user-login.event';
 import { RecipeCreatedEvent } from '../recipe/events/recipe-created.event';
 import { UserCreatedEvent } from '../user/events/user-created.event';
 import { TokenCount } from './schemas/token-count.schema';
@@ -24,8 +25,8 @@ export class TokensService {
     );
     try {
       await this.tokenCountModel.create({
-        userId: event.userDocument._id,
-        count: 3,
+        userId: event.userDocument.id,
+        tokens: 3,
       });
     } catch (error: any) {
       throw new InternalServerErrorException({
@@ -36,13 +37,13 @@ export class TokensService {
   }
 
   @OnEvent('user.login')
-  async handleUserLoginEvent(event: UserCreatedEvent) {
+  async handleUserLoginEvent(event: UserLoginEvent) {
     this.logger.log(
       `User login event received for user ${event.userDocument.email}.`,
     );
     try {
       const tokenCountDocument = await this.tokenCountModel.findOne({
-        userId: event.userDocument._id,
+        userId: event.userDocument.id,
       });
 
       if (!tokenCountDocument) {
@@ -50,8 +51,8 @@ export class TokensService {
           `No token count document found for user ${event.userDocument.email}. Creating one.`,
         );
         await this.tokenCountModel.create({
-          userId: event.userDocument._id,
-          count: 3,
+          userId: event.userDocument.id,
+          tokens: 3,
         });
       }
     } catch (error: any) {
@@ -79,15 +80,18 @@ export class TokensService {
     this.logger.log(
       `Recipe created event received for recipe ${event.recipeDocument.name}.`,
     );
+    const userId = event.userDocument.id;
+
     try {
-      const tokenCountDocument = await this.tokenCountModel.findOne({
-        userId: event.recipeDocument.userId,
-      });
-      tokenCountDocument.tokens = tokenCountDocument.tokens - 1;
-      await tokenCountDocument.save();
+      const tokenCountDocument = await this.tokenCountModel.findOneAndUpdate(
+        { userId },
+        { $inc: { tokens: -1 } },
+        { new: true },
+      );
+      return tokenCountDocument.tokens;
     } catch (error: any) {
       throw new InternalServerErrorException({
-        message: `Error updating token count for user ${event.recipeDocument.userId}.`,
+        message: `Error updating token count for user ${userId}.`,
         cause: error,
       });
     }
